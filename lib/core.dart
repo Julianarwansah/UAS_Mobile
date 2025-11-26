e7097d411385b11644b4c2023498cc80da54d0a1
NamaAnda <julianarwansahh@gmail.com>
2025-11-26 16:45:00 +0700

    menambahkan beberapa file

---
+++ lib/core.dart
@@ -0,0 +1,8 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+fileCount: 2
+*/
+export 'package:uas_mobile/core_package.dart';
+export 'package:uas_mobile/page/home.dart';
